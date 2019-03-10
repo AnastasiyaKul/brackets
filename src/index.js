cc9ad3807db9d1b1442e8a54d86093770b3aa086
AnastasiyaKul <43112477+AnastasiyaKul@users.noreply.github.com>
2019-03-10 22:33:21 +0300

    e

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  if(str.length % 2 !== 0) {return false}
+  let stack = [];
+  let bracketsMap = new Map();
+  for(let i = 0; i < bracketsConfig.length; i++) {
+    bracketsMap.set(bracketsConfig[i][0], bracketsConfig[i][1])
+  }
+
+  for(let i = 0; i < str.length; i++) {
+    if(bracketsMap.has(str[i])) {
+      if(str[i] === bracketsMap.get(str[i]) && str[i] === stack[stack.length - 1]) {
+        let del = stack.pop();
+      } else {
+        stack.push(str[i]);
+      }
+    } else {
+      if(stack.length !== 0) {
+        if(str[i] === bracketsMap.get(stack[stack.length - 1])) {
+          let del = stack.pop();
+        } else return false;
+      } else return false;
+    }
+  }
+
+  return stack.length === 0;
+};
